ba8b6caefb1d0bcf0b0e4f21302106d31d2cf4cc
kolaflash <kolaflash@GAMBIT>
2023-10-02 00:07:57 +0200

    Add cytoscape

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,55 @@
+/// <reference lib="dom" />
+/// <reference lib="dom.iterable" />
+
 import init, { greet } from "../public/build/graph_theory.js";
+import cytoscape from "cytoscape";
 
 await init();
 greet();
+
+cytoscape({
+  container: document.getElementById("app"),
+  elements: [
+    {
+      data: {
+        id: "a",
+      },
+    },
+    {
+      data: {
+        id: "b",
+      },
+    },
+    {
+      data: {
+        id: "ab",
+        source: "a",
+        target: "b",
+      },
+    },
+  ],
+  style: [
+    {
+      selector: "node",
+      style: {
+        "label": "data(id)",
+        "color": "#8fa1b2",
+        "background-color": "#df80ff ",
+      },
+    },
+    {
+      selector: "edge",
+      style: {
+        "width": 3,
+        "line-color": "#46afe3",
+        "target-arrow-color": "#46afe3",
+        "target-arrow-shape": "triangle",
+        "curve-style": "bezier",
+      },
+    },
+  ],
+  layout: {
+    name: "grid",
+    rows: 1,
+  },
+});
--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,11 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 extern "C" {
-    fn alert(s: &str);
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
 }
 
 #[wasm_bindgen]
 pub fn greet() {
-    alert("Hello, World!");
+    log("Hello, World!");
 }
