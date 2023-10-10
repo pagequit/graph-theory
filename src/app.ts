9c5407067243352a26d3b2a5ddb69d9726e0ce7b
kolaflash <kolaflash@GAMBIT>
2023-10-10 22:58:40 +0200

    WIP

--- src/app.ts
+++ src/app.ts
@@ -7,34 +7,15 @@ import cytoscape from "cytoscape";
 await init();
 greet();
 
-cytoscape({
+const cy = cytoscape({
   container: document.getElementById("app"),
-  elements: [
-    {
-      data: {
-        id: "a",
-      },
-    },
-    {
-      data: {
-        id: "b",
-      },
-    },
-    {
-      data: {
-        id: "ab",
-        source: "a",
-        target: "b",
-      },
-    },
-  ],
   style: [
     {
       selector: "node",
       style: {
         "label": "data(id)",
         "color": "#8fa1b2",
-        "background-color": "#df80ff ",
+        "background-color": "#46afe3",
       },
     },
     {
@@ -47,9 +28,62 @@ cytoscape({
         "curve-style": "bezier",
       },
     },
+    {
+      selector: ".highlighted",
+      style: {
+        "background-color": "#df80ff",
+        "line-color": "#df80ff",
+        "target-arrow-color": "#df80ff",
+        "transition-property":
+          "background-color, line-color, target-arrow-color",
+        "transition-duration": "0.3s",
+      },
+    },
   ],
+  elements: {
+    nodes: [
+      { data: { id: "a" } },
+      { data: { id: "b" } },
+      { data: { id: "c" } },
+      { data: { id: "d" } },
+      { data: { id: "e" } },
+      { data: { id: "f" } },
+    ],
+
+    edges: [
+      { data: { id: "ae", weight: 1, source: "a", target: "e" } },
+      { data: { id: "ab", weight: 3, source: "a", target: "b" } },
+      { data: { id: "be", weight: 4, source: "b", target: "e" } },
+      { data: { id: "bc", weight: 5, source: "b", target: "c" } },
+      { data: { id: "ce", weight: 6, source: "c", target: "e" } },
+      { data: { id: "cd", weight: 2, source: "c", target: "d" } },
+      { data: { id: "de", weight: 7, source: "d", target: "e" } },
+      { data: { id: "bf", weight: 2, source: "b", target: "f" } },
+    ],
+  },
   layout: {
-    name: "grid",
-    rows: 1,
+    name: "breadthfirst",
+    directed: true,
+    roots: "#a",
+    padding: 10,
   },
 });
+
+cy.getElementById("a").addClass("highlighted");
+cy.getElementById("ab").addClass("highlighted");
+cy.getElementById("b").addClass("highlighted");
+
+// const bfs = cy.elements().bfs("#a", () => {}, true);
+
+// let i = 0;
+// const highlightNextEle = function () {
+//   if (i < bfs.path.length) {
+//     bfs.path[i].addClass("highlighted");
+
+//     i++;
+//     setTimeout(highlightNextEle, 1000);
+//   }
+// };
+
+// // kick off first highlight
+// highlightNextEle();
